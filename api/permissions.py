300ba492d590db8b2aaac52d66fa5575ecd8b631
TimDzen <140600844+TimDzen@users.noreply.github.com>
2024-08-08 20:33:12 +0300

    Update permissions.py

--- api/permissions.py
+++ api/permissions.py
@@ -2,6 +2,11 @@ from rest_framework import permissions
 
 
 class IsProviderOrReadOnly(permissions.BasePermission):
+     """
+    Разрешение для поставщиков или только для чтения.
+    Позволяет изменять ресурсы только пользователям с типом `provider` (user_type == 1).
+    Все остальные пользователи могут только просматривать ресурсы.
+    """
     def has_permission(self, request, view):
         if request.user and request.user.is_authenticated:
             return bool(request.user.user_type == 1)
@@ -9,6 +14,11 @@ class IsProviderOrReadOnly(permissions.BasePermission):
 
 
 class IsCustumerOrReadOnly(permissions.BasePermission):
+     """
+    Разрешение для клиентов или только для чтения.
+    Позволяет изменять ресурсы только пользователям с типом `customer` (user_type == 2).
+    Все остальные пользователи могут только просматривать ресурсы.
+    """
     def has_permission(self, request, view):
         if request.user and request.user.is_authenticated:
             return bool(request.user.user_type == 2)
